3299fe3d287d13b4bd4d591d53b1e23f5e0a7828
ohbeomho <ohbeomho0322@gmail.com>
2024-09-24 22:14:31 +0900

    Little bit of UI change
    added new files!

--- morse-to-english.js
+++ morse-to-english.js
@@ -12,19 +12,13 @@ const content = document.getElementById("content"),
   morse_preview = document.getElementById("morse-preview"),
   letter_preview = document.getElementById("letter-preview")
 
-let currMorse = ""
-
 let downTime, inputTimeout
 /**
  *
  * @param {KeyboardEvent} e
  */
 window.onkeydown = (e) => {
-  if (e.key === "Backspace" && content.innerText.length)
-    content.innerText = content.innerText.substring(
-      0,
-      content.innerText.length - 1,
-    )
+  if (e.key === "Backspace" && content.innerHTML.length) content.innerHTML = content.innerHTML.substring(0, content.innerHTML.length - 1)
   else if (e.key == " " && !e.repeat) {
     downTime = performance.now()
     clearTimeout(inputTimeout)
@@ -39,18 +33,21 @@ window.onkeyup = (e) => {
   if (e.key !== " ") return
 
   const currTime = performance.now()
-  if (currTime - downTime < 200) morse_preview.innerText += "."
-  else morse_preview.innerText += "-"
+  if (currTime - downTime < 200) morse_preview.innerHTML += "."
+  else morse_preview.innerHTML += "-"
 
-  const letter = morse_letter[morse_preview.innerText]
-  letter_preview.innerText = letter
+  if (morse_preview.innerHTML.startsWith("&nbsp;")) morse_preview.innerHTML = morse_preview.innerHTML.substring(6)
+
+  const letter = morse_letter[morse_preview.innerHTML]
+  letter_preview.innerHTML = letter || "&nbsp;"
+  if (letter_preview.innerHTML.startsWith("&nbsp;")) letter_preview.innerHTML = letter_preview.innerHTML.substring(6)
 
   inputTimeout = setTimeout(() => {
-    const letter = morse_letter[morse_preview.innerText]
-    morse_preview.innerText = ""
-    letter_preview.innerText = ""
+    const letter = morse_letter[morse_preview.innerHTML]
+    morse_preview.innerHTML = "&nbsp;"
+    letter_preview.innerHTML = "&nbsp;"
     if (!letter) return
 
-    content.innerText += letter
+    content.innerHTML += letter
   }, 700)
 }
