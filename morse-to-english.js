301e783f8bb21ec9015ac5c1ba4a742e4b8747f1
ohbeomho <ohbeomho0322@gmail.com>
2024-09-24 22:36:01 +0900

    Added morse content view
    Also &nbsp; checking code is simpler

--- morse-to-english.js
+++ morse-to-english.js
@@ -1,5 +1,3 @@
-// TODO: Also make english to morse code translator
-
 // letter_morse -> key is letter
 // morse_letter -> key is morse code
 import letter_morse from "./morse.js"
@@ -9,8 +7,11 @@ for (let key in letter_morse) morse_letter[letter_morse[key]] = key
 console.log(morse_letter)
 
 const content = document.getElementById("content"),
+  content_morse = document.getElementById("content-morse"),
   morse_preview = document.getElementById("morse-preview"),
   letter_preview = document.getElementById("letter-preview")
+
+const morse_content = []
 
 let downTime, inputTimeout
 /**
@@ -18,8 +19,11 @@ let downTime, inputTimeout
  * @param {KeyboardEvent} e
  */
 window.onkeydown = (e) => {
-  if (e.key === "Backspace" && content.innerHTML.length) content.innerHTML = content.innerHTML.substring(0, content.innerHTML.length - 1)
-  else if (e.key == " " && !e.repeat) {
+  if (e.key === "Backspace" && content.innerHTML.length) {
+    content.innerHTML = content.innerHTML.substring(0, content.innerHTML.length - 1) || "&nbsp;"
+    morse_content.pop()
+    content_morse.innerHTML = morse_content.join("&nbsp;") || "&nbsp;"
+  } else if (e.key === " " && !e.repeat) {
     downTime = performance.now()
     clearTimeout(inputTimeout)
   }
@@ -32,22 +36,27 @@ window.onkeydown = (e) => {
 window.onkeyup = (e) => {
   if (e.key !== " ") return
 
+  if (morse_preview.innerHTML === "&nbsp;") morse_preview.innerHTML = ""
+
   const currTime = performance.now()
+  // TODO: 누르는 시간, 입력까지 기다리는 시간 설정 가능하게 만들기
   if (currTime - downTime < 200) morse_preview.innerHTML += "."
   else morse_preview.innerHTML += "-"
 
-  if (morse_preview.innerHTML.startsWith("&nbsp;")) morse_preview.innerHTML = morse_preview.innerHTML.substring(6)
-
   const letter = morse_letter[morse_preview.innerHTML]
+  if (letter_preview.innerHTML === "&nbsp;") letter_preview.innerHTML = ""
   letter_preview.innerHTML = letter || "&nbsp;"
-  if (letter_preview.innerHTML.startsWith("&nbsp;")) letter_preview.innerHTML = letter_preview.innerHTML.substring(6)
 
   inputTimeout = setTimeout(() => {
-    const letter = morse_letter[morse_preview.innerHTML]
+    const morse = morse_preview.innerHTML
+    const letter = morse_letter[morse]
     morse_preview.innerHTML = "&nbsp;"
     letter_preview.innerHTML = "&nbsp;"
     if (!letter) return
 
+    if (content.innerHTML === "&nbsp;") content.innerHTML = ""
     content.innerHTML += letter
+    morse_content.push(morse)
+    content_morse.innerHTML = morse_content.join("&nbsp;")
   }, 700)
 }
